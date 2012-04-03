da18483db85eecb3abce026d3c07deb738d23f82
Jesse Long <jpl@unknown.za.net>
2012-04-03 11:40:44 +0200

    Extract LogChute to separate class, and enable character set configuration

--- src/main/java/net/rumati/maven/plugins/velocity/VelocityMojo.java
+++ src/main/java/net/rumati/maven/plugins/velocity/VelocityMojo.java
@@ -1,8 +1,5 @@
 package net.rumati.maven.plugins.velocity;
 
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -11,13 +8,12 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
-import java.net.URL;
 import java.nio.charset.Charset;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
-import org.apache.velocity.runtime.RuntimeServices;
-import org.apache.velocity.runtime.log.LogChute;
 
 /**
  * Processes a Velocity template
@@ -48,10 +44,34 @@ public class VelocityMojo
      * @required
      */
     private File outputFile;
+    
+    /**
+     * The character set encoding to be used when reading and writing files.
+     * If this is not set, then {@code project.build.sourceEncoding} is used.
+     * If {@code project.build.sourceEncoding} is also not set, then the default
+     * character set encoding is used.
+     * @parameter expression="${velocity-maven-plugin.encoding}"
+     */
+    private String encoding;
 
     public void execute()
         throws MojoExecutionException
     {
+        Charset characterSet = null;
+        
+        if (encoding == null){
+            encoding = project.getProperties().getProperty("project.build.sourceEncoding");
+        }
+        
+        if (encoding == null){
+            getLog().warn("Using default character set encoding");
+            characterSet = Charset.defaultCharset();
+        }else{
+            characterSet = Charset.forName(encoding);
+        }
+
+        getLog().debug("Using character set: " + characterSet.displayName());
+        
         File parentDirectory = outputFile.getParentFile();
         if (!parentDirectory.isDirectory() && !parentDirectory.mkdirs()){
             throw new MojoExecutionException("Error creating output directory: " + parentDirectory.getAbsolutePath());
@@ -61,59 +81,17 @@ public class VelocityMojo
             InputStream templateStream;
             templateStream = this.getClass().getResourceAsStream(template);
             if (templateStream == null) {
-                getLog().debug("Could not find a resource called " + template);
+                getLog().debug("Could not find a resource called " + template + ", trying as a file name");
                 templateStream = new FileInputStream(template);
             } else {
                 getLog().debug("Using resource called " + template);
             }
-            Reader reader = new InputStreamReader(templateStream, Charset.forName("UTF-8"));
+            Reader reader = new InputStreamReader(templateStream, characterSet);
             try {
-                Writer writer = new OutputStreamWriter(new FileOutputStream(outputFile), Charset.forName("UTF-8"));
+                Writer writer = new OutputStreamWriter(new FileOutputStream(outputFile), characterSet);
                 try {
                     VelocityEngine engine = new VelocityEngine();
-                    engine.setProperty(VelocityEngine.RUNTIME_LOG_LOGSYSTEM, new LogChute() {
-
-                        public void init(RuntimeServices rs)
-                                throws Exception
-                        {
-                            /* do nothing */
-                        }
-
-                        public void log(int i, String string)
-                        {
-                            switch (i){
-                                case LogChute.INFO_ID:
-                                    VelocityMojo.this.getLog().info(string);
-                                    break;
-                                case LogChute.WARN_ID:
-                                    VelocityMojo.this.getLog().warn(string);
-                                    break;
-                                case LogChute.ERROR_ID:
-                                    VelocityMojo.this.getLog().error(string);
-                                    break;
-                            }
-                        }
-
-                        public void log(int i, String string, Throwable thrwbl)
-                        {
-                            switch (i){
-                                case LogChute.INFO_ID:
-                                    VelocityMojo.this.getLog().info(string, thrwbl);
-                                    break;
-                                case LogChute.WARN_ID:
-                                    VelocityMojo.this.getLog().warn(string, thrwbl);
-                                    break;
-                                case LogChute.ERROR_ID:
-                                    VelocityMojo.this.getLog().error(string, thrwbl);
-                                    break;
-                            }
-                        }
-
-                        public boolean isLevelEnabled(int i)
-                        {
-                            return i > LogChute.DEBUG_ID;
-                        }
-                    });
+                    engine.setProperty(VelocityEngine.RUNTIME_LOG_LOGSYSTEM, new MavenLogChute(getLog()));
                     engine.init();
                     VelocityContext ctx = new VelocityContext();
                     ctx.put("project", project);
