fbdfa033b39181881053d3eb7767f82339e63554
Jesse Long <jpl@unknown.za.net>
2014-10-02 13:39:15 +0200

    Add ability to include

--- src/main/java/net/rumati/maven/plugins/velocity/VelocityMojo.java
+++ src/main/java/net/rumati/maven/plugins/velocity/VelocityMojo.java
@@ -9,11 +9,14 @@ import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
 import java.nio.charset.Charset;
+import java.util.Properties;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+import org.apache.velocity.runtime.resource.loader.FileResourceLoader;
 
 /**
  * Processes a Velocity template
@@ -23,7 +26,7 @@ import org.apache.velocity.app.VelocityEngine;
  * @requiresDependencyResolution test
  */
 public class VelocityMojo
-	extends AbstractMojo
+        extends AbstractMojo
 {
     /**
      * @parameter property="project"
@@ -59,76 +62,87 @@ public class VelocityMojo
     private Transformation[] transformations;
 
     public void execute()
-	    throws MojoExecutionException
+            throws MojoExecutionException
     {
-	Charset characterSet = null;
+        Charset characterSet = null;
 
-	if (encoding == null){
-	    encoding = project.getProperties().getProperty("project.build.sourceEncoding");
-	}
+        if (encoding == null){
+            encoding = project.getProperties().getProperty("project.build.sourceEncoding");
+        }
 
-	if (encoding == null){
-	    getLog().warn("Using default character set encoding");
-	    characterSet = Charset.defaultCharset();
-	}else{
-	    characterSet = Charset.forName(encoding);
-	}
+        if (encoding == null){
+            getLog().warn("Using default character set encoding");
+            characterSet = Charset.defaultCharset();
+        }else{
+            characterSet = Charset.forName(encoding);
+        }
 
-	getLog().debug("Using character set: " + characterSet.displayName());
+        getLog().debug("Using character set: " + characterSet.displayName());
 
-	if (template != null && outputFile != null){
-	    Transformation t = new Transformation();
-	    t.outputFile = outputFile;
-	    t.template = template;
-	    transform(t, characterSet);
-	}
+        if (template != null && outputFile != null){
+            Transformation t = new Transformation();
+            t.outputFile = outputFile;
+            t.template = template;
+            transform(t, characterSet);
+        }
 
-	if (transformations != null){
-	    for (Transformation t : transformations){
-		if (t.outputFile != null && t.template != null){
-		    transform(t, characterSet);
-		}
-	    }
-	}
+        if (transformations != null){
+            for (Transformation t : transformations){
+                if (t.outputFile != null && t.template != null){
+                    transform(t, characterSet);
+                }
+            }
+        }
     }
 
     private void transform(Transformation transformation, Charset characterSet)
-	    throws MojoExecutionException
+            throws MojoExecutionException
     {
-	File parentDirectory = transformation.outputFile.getParentFile();
-	if (!parentDirectory.isDirectory() && !parentDirectory.mkdirs()){
-	    throw new MojoExecutionException("Error creating output directory: " + parentDirectory.getAbsolutePath());
-	}
+        File parentDirectory = transformation.outputFile.getParentFile();
+        if (!parentDirectory.isDirectory() && !parentDirectory.mkdirs()){
+            throw new MojoExecutionException("Error creating output directory: " + parentDirectory.getAbsolutePath());
+        }
 
-	try {
-	    InputStream templateStream;
-	    templateStream = this.getClass().getResourceAsStream(transformation.template);
-	    if (templateStream == null){
-		getLog().debug("Could not find a resource called " + transformation.template + ", trying as a file name");
-		templateStream = new FileInputStream(transformation.template);
-	    }else{
-		getLog().debug("Using resource called " + transformation.template);
-	    }
-	    Reader reader = new InputStreamReader(templateStream, characterSet);
-	    try {
-		Writer writer = new OutputStreamWriter(new FileOutputStream(transformation.outputFile), characterSet);
-		try {
-		    VelocityEngine engine = new VelocityEngine();
-		    engine.setProperty(VelocityEngine.RUNTIME_LOG_LOGSYSTEM, new MavenLogChute(getLog()));
-		    engine.init();
-		    VelocityContext ctx = new VelocityContext();
-		    ctx.put("project", project);
-		    ctx.put("system", System.getProperties());
-		    ctx.put("env", System.getenv());
-		    engine.evaluate(ctx, writer, "velocity-maven-plugin", reader);
-		}finally{
-		    writer.close();
-		}
-	    }finally{
-		reader.close();
-	    }
-	}catch (Exception e){
-	    throw new MojoExecutionException("Error processing template", e);
-	}
+        try {
+            File fileResourceDirectory = null;
+            InputStream templateStream;
+            templateStream = this.getClass().getResourceAsStream(transformation.template);
+            if (templateStream == null){
+                getLog().debug("Could not find a resource called " + transformation.template + ", trying as a file name");
+                templateStream = new FileInputStream(transformation.template);
+                fileResourceDirectory = new File(transformation.template).getAbsoluteFile().getParentFile();
+            }else{
+                getLog().debug("Using resource called " + transformation.template);
+            }
+            Reader reader = new InputStreamReader(templateStream, characterSet);
+            try {
+                Writer writer = new OutputStreamWriter(new FileOutputStream(transformation.outputFile), characterSet);
+                try {
+                    VelocityEngine engine = new VelocityEngine();
+                    Properties engineConfig = new Properties();
+                    engineConfig.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+                    if (fileResourceDirectory == null){
+                        engineConfig.setProperty("resource.loader", "classpath");
+                    }else{
+                        engineConfig.setProperty("resource.loader", "classpath,file");
+                        engineConfig.setProperty("file.resource.loader.class", FileResourceLoader.class.getName());
+                        engineConfig.setProperty("file.resource.loader.path", fileResourceDirectory.getAbsolutePath());
+                    }
+                    engine.setProperty(VelocityEngine.RUNTIME_LOG_LOGSYSTEM, new MavenLogChute(getLog()));
+                    engine.init(engineConfig);
+                    VelocityContext ctx = new VelocityContext();
+                    ctx.put("project", project);
+                    ctx.put("system", System.getProperties());
+                    ctx.put("env", System.getenv());
+                    engine.evaluate(ctx, writer, "velocity-maven-plugin", reader);
+                }finally{
+                    writer.close();
+                }
+            }finally{
+                reader.close();
+            }
+        }catch (Exception e){
+            throw new MojoExecutionException("Error processing template", e);
+        }
     }
 }
